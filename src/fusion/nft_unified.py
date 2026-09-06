96015981d8bdb7bd351458af89a765fe1925279f
christianscode1994 <fourieranalys@hotmail.com>
2026-09-06 17:41:24 +0300

    Import functions from MagicEden modules

---
+++ src/fusion/nft_unified.py
@@ -0,0 +1,4 @@
+from src.nft.solana.magiceden_collections import get_collection_stats
+from src.nft.solana.magiceden_listings import get_listings
+from src.nft.solana.magiceden_wallets import get_wallet_tokens
+from src.nft.solana.magiceden_activity import get_activity
