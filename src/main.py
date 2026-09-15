ad1e863c68213b89cd67f4773987881d78437941
christianscode1994 <fourieranalys@hotmail.com>
2026-09-15 17:39:09 +0300

    Add SnapshotReader import to main.py

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from .generate_output import build_payload
 from .history import write_snapshot
 from .dashboard import print_dashboard
 from .metrics import aggregate_all_metrics, volatai_score, detect_alerts
+from .snapshot_reader import SnapshotReader
 
 from src.onchain.rpc import RPC
 from src.onchain.intelligence import (
