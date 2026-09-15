a1ecd0caec32e0004b1a6302299cb8488591b189
christianscode1994 <fourieranalys@hotmail.com>
2026-09-15 22:49:01 +0300

    Change RPC provider from 'public_eth' to 'ethereum'

--- src/main.py
+++ src/main.py
@@ -42,7 +42,8 @@ def run_once(write_snaps: bool, show_dashboard: bool):
 
     # On-chain
     rpc = RPC()
-    latest_block = rpc.get_block_number(provider="public_eth")
+    latest_block = rpc.get_block_number(provider="ethereum")
+
 
     from_block = max(0, latest_block - 100)
     to_block = latest_block
