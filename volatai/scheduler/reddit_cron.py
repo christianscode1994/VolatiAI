5516b9de2498c2a432ed9163ddf0d6473bc6640b
christianscode1994 <fourieranalys@hotmail.com>
2026-09-26 18:02:27 +0300

    Add RedditScheduler class for scheduling tasks

---
+++ volatai/scheduler/reddit_cron.py
@@ -0,0 +1,15 @@
+from .base_scheduler import build_daily_messages, build_event_messages
+from volatai.bots.reddit.reddit_adapter import render_summary, render_events
+
+class RedditScheduler:
+    def daily(self):
+        return build_daily_messages(adapter=self)
+
+    def events(self):
+        return build_event_messages(adapter=self)
+
+    def render_summary(self, summary):
+        return render_summary(summary)
+
+    def render_events(self, events):
+        return render_events(events)
