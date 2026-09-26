11f8f37bd64fd72323e19674cd2e9ec30decb794
christianscode1994 <fourieranalys@hotmail.com>
2026-09-26 18:12:07 +0300

    Add MatrixScheduler class for scheduling tasks

---
+++ scheduler/matrix_cron.py
@@ -0,0 +1,15 @@
+from .base_scheduler import build_daily_messages, build_event_messages
+from volatai.bots.matrix.matrix_adapter import render_summary, render_events
+
+class MatrixScheduler:
+    def daily(self):
+        return build_daily_messages(adapter=self)
+
+    def events(self):
+        return build_event_messages(adapter=self)
+
+    def render_summary(self, summary):
+        return render_summary(summary)
+
+    def render_events(self, events):
+        return render_events(events)
