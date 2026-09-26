523c19f5253ea94b7b0e19f2ea66f34ee578e7dd
christianscode1994 <fourieranalys@hotmail.com>
2026-09-26 18:00:13 +0300

    Add DiscordScheduler class for scheduling tasks

---
+++ volatai/scheduler/discord_cron.py
@@ -0,0 +1,15 @@
+from .base_scheduler import build_daily_messages, build_event_messages
+from volatai.bots.discord.discord_adapter import render_summary, render_events
+
+class DiscordScheduler:
+    def daily(self):
+        return build_daily_messages(adapter=self)
+
+    def events(self):
+        return build_event_messages(adapter=self)
+
+    def render_summary(self, summary):
+        return render_summary(summary)
+
+    def render_events(self, events):
+        return render_events(events)
