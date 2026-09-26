4854151b9df481dd7b014ea1ad3e6f73a0156971
christianscode1994 <fourieranalys@hotmail.com>
2026-09-26 17:56:30 +0300

    Implement TelegramScheduler with daily and events methods

---
+++ volatai/scheduler/telegram_cron.py
@@ -0,0 +1,16 @@
+from .base_scheduler import build_daily_messages, build_event_messages
+from volatai.bots.telegram.telegram_adapter import render_summary, render_events
+
+class TelegramScheduler:
+    def daily(self):
+        return build_daily_messages(adapter=self)
+
+    def events(self):
+        return build_event_messages(adapter=self)
+
+    # Adapter interface
+    def render_summary(self, summary):
+        return render_summary(summary)
+
+    def render_events(self, events):
+        return render_events(events)
